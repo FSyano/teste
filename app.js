58ed1b144cd1d522fb873cc1a4834cc5b2a1098b
Felipe Yano <felipe-yano@hotmail.com>
2018-09-24 22:39:32 -0300

    Ajuste google

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 var app = require('express')();
 var cfenv = require('cfenv');
+const bodyParser = require('body-parser')
 const {dialogflow, SimpleResponse} = require('actions-on-google')
 
 appEnv = cfenv.getAppEnv()
 instance = appEnv.app.instance_index ||  0
 
-dialogflowApp = dialogflow()
+var dialogflowApp = dialogflow()
 
 dialogflowApp.intent('Default Fallback Intent', (conv)=>{
+    console.log('aqui input: ')
+    console.log(conv.request.inputs[0].rawInputs[0].query)
     conv.ask(new SimpleResponse({
         text: 'texto do node',
         speech: 'texto do node'
     }))
 })
+
+app.use(bodyParser.json())
 
 app.listen(appEnv.port, function(){
     console.log('Servidor node no ar')
